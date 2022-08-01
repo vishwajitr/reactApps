5713cd11c6dd803a216e5cdc43839e572a32b89c
vishwajit <vishwajitrikamme@gmail.com>
2022-08-01 20:22:08 +0530

    todo app with localStorage

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Todo from './todos/todo';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Todo title="Daily Todo's" />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/todos/todo.js
@@ -0,0 +1,86 @@
+import '../css/todo.css'
+import { useState, useEffect } from "react"
+
+const Todo = (props) => {
+    const [inputText, setinputText] = useState("");
+    const [todos, setTodo] = useState(JSON.parse(localStorage.getItem('todos')) || []
+    )
+    const [updatedText, setupdatedText] = useState("");
+    const [todoEditing, settodoEditing] = useState("");
+
+    useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify(todos));
+    }, [todos]);
+
+
+    const handlerSubmit = (e) => {
+        e.preventDefault();
+        addTodo(inputText);
+    }
+    const addTodo = (title) => {
+        console.log(title)
+        const newTodo = [...todos, { 'title': title, 'status': false, id: Math.random() * 10 }]
+        setTodo(newTodo)
+        localStorage.setItem(newTodo);
+    }
+
+
+    const updatedTextHandler = (e) => { setupdatedText(e.target.value) }
+
+    const editHandler = (id, text) => {
+        settodoEditing(id);
+        setupdatedText(text)
+    }
+
+    const updateHandler = (id) => {
+        setTodo(todos.map(todo => {
+            if (todo.id == id) {
+                return { ...todo, title: updatedText }
+            }
+            return todo
+        }))
+        settodoEditing(null);
+    }
+
+    const deleteHandler = (id) => {
+        const removeTodo = todos.filter((todo) => {
+            return todo.id !== id
+        })
+        setTodo(removeTodo);
+    }
+
+    return (
+        <div>
+            <div className="todoApp">
+                <h1>{props.title}</h1>
+                <form onSubmit={handlerSubmit}>
+                    <input type="text" name="todoText" onChange={(e) => { setinputText(e.target.value) }} />
+                    <button type="submit">Add Todo</button>
+                </form>
+                <ul>
+                    {
+                        todos.map(function (todo, index) {
+                            return (
+                                <li key={index}>
+                                    {(todoEditing !== todo.id) && todo.title}
+                                    {(todoEditing == todo.id) && <input type="text" name="editText" value={updatedText} onChange={updatedTextHandler} />}
+                                    <div className="actions">
+                                        {(todoEditing == todo.id) && <button onClick={() => { updateHandler(todo.id) }}>save</button>}
+                                        {(todoEditing !== todo.id) && <button onClick={() => { editHandler(todo.id, todo.title) }}>edit</button>}
+                                        <button onClick={() => { deleteHandler(todo.id) }}>Delete</button>
+                                    </div>
+                                </li>
+                            )
+                        })
+
+                    }
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+
+
+
+export default Todo;
